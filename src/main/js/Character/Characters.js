c041dc3fd168e7208f4bd703ec5d84e4e0b0444d
Pablo <pablomarinpro@gmail.com>
2023-04-11 14:55:35 +0200

    [FUNC] Importar de DBPedia
    Se puede buscar un personaje de DBPedia y autocompletar esos datos

--- src/main/js/Character/Characters.js
+++ src/main/js/Character/Characters.js
@@ -3,12 +3,13 @@ const ReactDOM = require("react-dom/client");
 import CloudUploadIcon from "@mui/icons-material/CloudUpload";
 import FilterHdrIcon from "@mui/icons-material/FilterHdr";
 import SearchIcon from "@mui/icons-material/Search";
+import Link from "@mui/material/Link";
 import {
   Card,
   CardActionArea,
   CardActions,
   CardContent,
-  CardHeader
+  CardHeader,
 } from "@mui/material";
 import Avatar from "@mui/material/Avatar";
 import Box from "@mui/material/Box";
@@ -23,13 +24,18 @@ import { createTheme, ThemeProvider } from "@mui/material/styles";
 import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
 import { Link as LinkRouter, useNavigate } from "react-router-dom";
+import CloseIcon from "@mui/icons-material/Close";
+import DataTable from "./NewWindow";
 
 import PersonAddAlt1Icon from "@mui/icons-material/PersonAddAlt1";
+
+import TravelExploreIcon from "@mui/icons-material/TravelExplore";
 
 import Badge from "@mui/material/Badge";
 import axios from "axios";
 
 import Footer from "../Footer";
+import Modal from "./Modal";
 import Navigation from "../Navigation";
 
 const theme = createTheme();
@@ -44,6 +50,12 @@ export default function Personajes() {
 
   const [showButton, setShowButton] = React.useState(null);
 
+  const [showDBSearch, setShowDBSearch] = React.useState(false);
+
+  const [showDBResults, setShowDBResults] = React.useState(false);
+
+  const [dBResults, setDBResults] = React.useState([]);
+
   const handleSearchChange = (event) => {
     const query = event.target.value;
     setSearchQuery(query);
@@ -52,6 +64,24 @@ export default function Personajes() {
       personaje.nombre.toLowerCase().includes(query.toLowerCase())
     );
     generarSeccionesPersonajes(filtered);
+  };
+
+  const handleSearchDBPedia = (event) => {
+    var query = document.getElementById("dbpedia-search").value;
+    query = query.split(" ").join("+");
+    axios
+      .get(
+        "https://dbpedia.org/sparql/?default-graph-uri=http%3A%2F%2Fdbpedia.org&query=PREFIX+foaf%3A+%3Chttp%3A%2F%2Fxmlns.com%2Ffoaf%2F0.1%2F%3E%0D%0APREFIX+dbo%3A+%3Chttp%3A%2F%2Fdbpedia.org%2Fontology%2F%3E%0D%0APREFIX+dbp%3A+%3Chttp%3A%2F%2Fdbpedia.org%2Fproperty%2F%3E%0D%0APREFIX+xsd%3A+%3Chttp%3A%2F%2Fwww.w3.org%2F2001%2FXMLSchema%23%3E%0D%0ASELECT+DISTINCT+%3Fnombre+%3Fapellidos+%3Fgenero+%3Fresidencia+%3Faltura+%3FfechaValida+%3FlugarNacimiento+%3Fdescripcion%0D%0AWHERE+%7B%0D%0A++%3Fpersona+foaf%3Aname+%3Fnombre+.%0D%0A++FILTER+regex%28%3Fnombre%2C+%22" +
+          query +
+          "%22%2C+%22i%22%29+.%0D%0A++OPTIONAL+%7B+%3Fpersona+foaf%3Asurname+%3Fapellidos+.+%7D%0D%0A++OPTIONAL+%7B+%3Fpersona+dbp%3Agender+%3Fgenero+.+%7D%0D%0A++OPTIONAL+%7B+%3Fpersona+dbo%3Aresidence+%3Fresidencia+.+%7D%0D%0A++OPTIONAL+%7B+%3Fpersona+dbo%3Aheight+%3Faltura+.+%7D%0D%0AOPTIONAL+%7B+%3Fpersona+dbp%3Aorigin+%3FlugarNacimiento+.+%7D%0D%0A++OPTIONAL+%7B+%0D%0A++++%3Fpersona+dbp%3Adata+%3FfechaNacimiento+.%0D%0A++++BIND%28IF%28STR%28%3FfechaNacimiento%29+%3D+REGEX%28STR%28%3FfechaNacimiento%29%2C+%22%5E%28%5C%5Cd%7B4%7D-%5C%5Cd%7B2%7D-%5C%5Cd%7B2%7D%7C%5C%5Cd%7B2%7D-%5C%5Cd%7B2%7D-%5C%5Cd%7B4%7D%7C%29%24%22%29%2C+%3FfechaNacimiento%2C+UNDEF%29+AS+%3FfechaValida%29%0D%0A++%7D%0D%0A++OPTIONAL+%7B+%3Fpersona+dbo%3Aabstract+%3Fdescripcion+.%0D%0A+++++++++++++FILTER+%28lang%28%3Fdescripcion%29+%3D+%22en%22+%7C%7C+lang%28%3Fdescripcion%29+%3D+%22es%22%29+.%0D%0A+++++++++++%7D%0D%0A++%3Fpersona+dbo%3AwikiPageID+%3Fid+.%0D%0A++%3Fpersona+a%2Frdfs%3AsubClassOf*+dbo%3AFictionalCharacter+.%0D%0A%7D&format=application%2Fsparql-results%2Bjson&timeout=10000&signal_void=on&signal_unconnected=on"
+      )
+      .then((response) => {
+        setDBResults(response.data.results.bindings);
+        setShowDBResults(true);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   };
 
   const handleImportInit = (event) => {
@@ -108,6 +138,10 @@ export default function Personajes() {
     localStorage.setItem("personaje", personajeGuardado);
     setTimeout(navigate("/personaje/info"), 20);
   }
+
+  const closeModal = () => {
+    setShowDBSearch(false);
+  };
 
   function generarSeccionesPersonajes(historiasFiltradas) {
     // Este es el objeto vacío que almacena las secciones
@@ -208,54 +242,141 @@ export default function Personajes() {
           </Container>
         </Box>
         <Container sx={{ py: 1 }} maxWidth="md">
-          {/* End hero unit */}
-          {(!personajes || personajes.length === 0) &&
-            showButton && (
-              <Grid container spacing={4} justifyContent="center">
-                <Button
-                  type="submit"
-                  variant="contained"
-                  startIcon={<PersonAddAlt1Icon />}
-                >
-                  {" "}
-                  <LinkRouter
-                    style={{ color: "#FFF", textDecoration: "none" }}
-                    to="/personajes/new"
-                  >
-                    ¡Crea tu personaje!
-                  </LinkRouter>
-                </Button>
-              </Grid>
-            )}
-          {personajes &&
-            personajes.length !== 0 &&
-            showButton && (
-              <Grid
-                container
-                spacing={4}
-                justifyContent="center"
-                sx={{ pb: 4 }}
+          <Grid
+            container
+            spacing={4}
+            justifyContent="center"
+            sx={{ pb: 4, ml: 0 }}
+          >
+            {/* End hero unit */}
+            {(!personajes || personajes.length === 0) && showButton && (
+              <Button
+                type="submit"
+                variant="contained"
+                startIcon={<PersonAddAlt1Icon />}
               >
-                <Button
-                  type="submit"
-                  variant="contained"
-                  startIcon={<PersonAddAlt1Icon />}
+                {" "}
+                <LinkRouter
+                  style={{ color: "#FFF", textDecoration: "none" }}
+                  to="/personajes/new"
                 >
-                  {" "}
-                  <LinkRouter
-                    style={{ color: "#FFF", textDecoration: "none" }}
-                    to="/personajes/new"
-                  >
-                    Crea un personaje
-                  </LinkRouter>
-                </Button>
-              </Grid>
+                  ¡Crea tu personaje!
+                </LinkRouter>
+              </Button>
             )}
+            {personajes && personajes.length !== 0 && showButton && (
+              <Button
+                type="submit"
+                variant="contained"
+                startIcon={<PersonAddAlt1Icon />}
+              >
+                {" "}
+                <LinkRouter
+                  style={{ color: "#FFF", textDecoration: "none" }}
+                  to="/personajes/new"
+                >
+                  Crea un personaje
+                </LinkRouter>
+              </Button>
+            )}
+            <Button
+              type="submit"
+              variant="contained"
+              startIcon={<TravelExploreIcon />}
+              sx={{ ml: 1 }}
+              onClick={() => setShowDBSearch(true)}
+            >
+              Importar de DBPedia
+            </Button>
+            {showDBSearch && (
+              <Modal>
+                <div
+                  className="modal-content"
+                  style={{
+                    width: "80%",
+                    backgroundColor: "white",
+                    padding: "20px",
+                    overflowY: "scroll",
+
+                  }}
+                >
+                  <Typography
+                    component="h3"
+                    variant="h5"
+                    align="left"
+                    color="text.primary"
+                    sx={{ mt: 2, ml: 2 }}
+                    gutterBottom
+                  >
+                    Importar de DBPedia
+                  </Typography>
+                  <Typography
+                    component="h5"
+                    variant="h5"
+                    align="left"
+                    color="text.primary"
+                    sx={{ mt: 2, ml: 2 }}
+                    gutterBottom
+                  >
+                    Información procedente de:
+                    <Link href={"https://dbpedia.org/"} sx={{ ml: 2 }}>
+                      <img
+                        src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/DBpediaLogo.svg/263px-DBpediaLogo.svg.png"
+                        alt="DBPedia"
+                        width="120"
+                        height="63"
+                      />
+                    </Link>
+                  </Typography>
+
+                  <TextField
+                    label="Buscar"
+                    fullWidth
+                    id="dbpedia-search"
+                    InputProps={{
+                      endAdornment: (
+                        <InputAdornment position="end">
+                          <SearchIcon />
+                        </InputAdornment>
+                      ),
+                    }}
+                  />
+
+                  {showDBResults && (
+                    <div>
+                      <DataTable data={dBResults} />
+                    </div>
+                  )}
+
+                  <Button
+                    variant="contained"
+                    sx={{ mt: 2, mr: 2 }}
+                    onClick={handleSearchDBPedia}
+                    startIcon={<SearchIcon />}
+                  >
+                    Buscar
+                  </Button>
+
+                  <Button
+                    variant="contained"
+                    color="error"
+                    onClick={closeModal}
+                    sx={{ mt: 2 }}
+                    startIcon={<CloseIcon />}
+                  >
+                    Cerrar
+                  </Button>
+                </div>
+              </Modal>
+            )}
+          </Grid>
           <Grid container>
             {Object.keys(personajesPorSecciones).map((seccion) => (
-                <><Typography variant="h5" color="text.primary" paragraph>
-                {seccion}
-              </Typography><Grid container spacing={4} sx={{pb:2}}>
+              <>
+                <Typography variant="h5" color="text.primary" paragraph>
+                  {seccion}
+                </Typography>
+                <Grid container spacing={4} sx={{ pb: 2 }}>
                   {personajesPorSecciones[seccion].map((personaje) => (
                     <Grid item key={personaje.id} xs={12} sm={6} md={4}>
                       <Card
@@ -265,11 +386,12 @@ export default function Personajes() {
                           flexDirection: "column",
                         }}
                       >
-                        <CardActionArea
-                          onClick={() => handleClick(personaje)}
-                        >
+                        <CardActionArea onClick={() => handleClick(personaje)}>
                           <CardHeader
-                            avatar={<Avatar alt="Apple" src={personaje.urlIcon} />} />
+                            avatar={
+                              <Avatar alt="Apple" src={personaje.urlIcon} />
+                            }
+                          />
 
                           <CardContent sx={{ flexGrow: 1 }}>
                             <Typography
@@ -300,7 +422,8 @@ export default function Personajes() {
                       </Card>
                     </Grid>
                   ))}
-                </Grid></>
+                </Grid>
+              </>
             ))}
           </Grid>
         </Container>
---
+++ src/main/js/Character/NewWindow.js
@@ -0,0 +1,114 @@
+const React = require("react");
+const ReactDOM = require("react-dom");
+
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TablePagination,
+} from "@mui/material";
+
+import Button from "@mui/material/Button";
+import { useNavigate } from "react-router-dom";
+
+function DataTable(props) {
+  // Obtener los datos del prop data
+  const data = props.data;
+
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+  const importData = (data) => {
+    setPage(newPage);
+  };
+
+  const navigate = useNavigate();
+
+  function handleImport(personaje) {
+    // Crear un nuevo objeto simple
+    var personaje_clean = {};
+
+    // Obtener las claves del objeto complejo
+    var claves = Object.keys(personaje);
+
+    // Iterar sobre las claves y asignar los valores al nuevo objeto
+    for (var i = 0; i < claves.length; i++) {
+      var clave = claves[i];
+      var valor = personaje[clave].value;
+      personaje_clean[clave] = valor;
+    }
+
+    // Mostrar el nuevo objeto simple
+    localStorage.setItem("personaje", JSON.stringify(personaje_clean));
+    setTimeout(navigate("/personaje/update"), 20);
+  }
+
+  // Renderizar una tabla con MaterialUI
+  return (
+    <Paper sx={{ width: "100%", overflow: "hidden" }}>
+      <TableContainer>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell sx={{ fontWeight: "bold" }}>Nombre</TableCell>
+              <TableCell sx={{ fontWeight: "bold" }}>Genero</TableCell>
+              <TableCell sx={{ fontWeight: "bold" }}>
+                Lugar de nacimiento
+              </TableCell>
+              <TableCell sx={{ fontWeight: "bold" }}>
+                Fecha de nacimiento
+              </TableCell>
+              <TableCell sx={{ fontWeight: "bold" }}>Importar</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((item, index) => (
+                <TableRow key={index}>
+                  <TableCell> {item.nombre?.value}</TableCell>
+                  <TableCell>{item.genero?.value}</TableCell>
+                  <TableCell>{item.lugarNacimiento?.value}</TableCell>
+                  <TableCell>{item.fechaNacimiento?.value}</TableCell>
+                  <TableCell>
+                    <Button
+                      type="submit"
+                      variant="contained"
+                      sx={{ ml: 1 }}
+                      onClick={() => handleImport(item)}
+                    >
+                      Importar de DBPedia
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[5, 10, 25, 100]}
+        component="div"
+        count={data.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Paper>
+  );
+}
+
+export default DataTable;
